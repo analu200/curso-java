83b83e7569d86caa45be02d7230ccf50f103e845
analu200 <49160241+analu200@users.noreply.github.com>
2019-04-16 12:18:38 -0500

    Add files via upload

---
+++ PracticaWhile2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author PC20-LAB02
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n=500;
+       int c=1;
+        while (c<=n){
+           System.out.println(c);
+           c=c+1;
+        }
+        
+    }
+    
+}
